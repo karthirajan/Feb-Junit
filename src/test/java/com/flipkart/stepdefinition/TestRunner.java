4a46052a94e7157bce7f7c82d173dc3d39be2a70
karthirajan <karthirajan0706@gmail.com>
2022-02-23 19:50:02 +0800

    Junit

---
+++ src/test/java/com/flipkart/stepdefinition/TestRunner.java
@@ -0,0 +1,15 @@
+package com.flipkart.stepdefinition;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({FlipkartMobile.class,FlipkartTv.class})
+
+
+public class TestRunner {
+
+	//Test runner is a suite class. we can decide which classes need to be execute by using testrunner
+	
+}
